1afde4682e03bac4924695c8de0107a0aed3ba96
felruizrojas <fe.ruizr@duocuc.cl>
2025-06-08 01:46:56 -0400

    repository

---
+++ src/main/java/com/micro1_usuarios/micro1_usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.micro1_usuarios.micro1_usuarios.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.micro1_usuarios.micro1_usuarios.model.Usuario;
+
+@Repository
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    // Aquí puedes agregar métodos personalizados si es necesario
+
+}
